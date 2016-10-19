86e39eadad16c96174168fcdda92287dea527ba7
ovarunendra <vpsingh016@gmail.com>
2016-10-19 14:41:05 +0530

    making it work with browser

---
+++ src/components/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { Question } from '../../models';
+
+@Component({
+    selector: 'dynamic-form-app',
+    template: require('./app.component.html')
+})
+export class AppComponent {
+    questions: Array<Question>;
+
+    constructor() {
+        this.questions = [];
+    }
+}

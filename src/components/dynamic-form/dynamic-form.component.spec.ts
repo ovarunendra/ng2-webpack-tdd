df1ca7087fecef53f5d81b342dcfdbb4c7e65849
ovarunendra <vpsingh016@gmail.com>
2016-10-19 13:32:14 +0530

    added more test

--- src/components/dynamic-form/dynamic-form.component.spec.ts
+++ src/components/dynamic-form/dynamic-form.component.spec.ts
@@ -30,4 +30,26 @@ describe('Component: DynamicFormComponent', () => {
         component.ngOnInit();
         expect(component.formGroup instanceof FormGroup).toBe(true);
     });
+
+    it('should create a `FormControl` for each question', () => {
+        component.questions = [
+            {
+                controlType: 'text',
+                id: 'first',
+                label: 'My First',
+                required: false
+            },
+            {
+                controlType: 'text',
+                id: 'second',
+                label: 'Second!',
+                required: true
+            }
+        ];
+        component.ngOnInit();
+
+        expect(Object.keys(component.formGroup.controls)).toEqual([
+            'first', 'second'
+        ]);
+    });
 })
--- src/components/dynamic-form/dynamic-form.component.ts
+++ src/components/dynamic-form/dynamic-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Question } from '../../models';
 
@@ -14,10 +14,22 @@ export class DynamicFormComponent implements OnInit {
     formGroup: FormGroup;
 
     ngOnInit() {
-        this.formGroup = this.generateForm(this.questions);
+        this.formGroup = this.generateForm(this.questions || []);
     }
 
     private generateForm(questions: Array<Question>): FormGroup {
-        return new FormGroup({});
+        const formControls = questions.reduce(this.generateControl, {});
+
+        return new FormGroup(formControls);
+    }
+
+    private generateControl(controls: any, question: Question) {
+        if (question.required) {
+            controls[question.id] = new FormControl(question.value || '', Validators.required);
+        } else {
+            controls[question.id] = new FormControl(question.value || '');
+        }
+
+        return controls;
     }
 }
